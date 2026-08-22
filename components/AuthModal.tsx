750c498dd7f25597b12b58ec226d71a300cf178a
SouptikSinha <souptiksinha1@gmail.com>
2026-08-22 14:24:17 -0500

    redirect OAuth to current page

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -42,7 +42,7 @@ export default function AuthModal({ isOpen, onClose }: AuthModalProps) {
     setError(null);
     await supabase.auth.signInWithOAuth({
       provider: "google",
-      options: { redirectTo: window.location.origin },
+      options: { redirectTo: window.location.href },
     });
   }
 
